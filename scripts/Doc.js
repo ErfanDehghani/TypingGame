9a7d9b40c92dae0c1163d9669cfc1112482728ce
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-11-14 14:44:41 +0330

    Reformated the code with prettier

--- scripts/Doc.js
+++ scripts/Doc.js
@@ -1,98 +1,90 @@
 /**
  * This Class is responsible for alternating the dom based on the how game goes
  */
-export default class Document
-{
-    // ScoreBoard properties ----------------------------------------------------
-    /**
-     * scoreBoard
-     * @property
-     * @type Element
-     */
-    scoreBoardElement = this.$('scoreBoard');
-    /**
-     * current game's word per minute counter element
-     * @property
-     * @type {Element}
-     */
-    wordPerMinuteElement = this.$("scoreBoard_WPM");
-    /**
-     * the current game's errors element'
-     * @property
-     * @type {Element}
-     */
-    errorsElement = this.$('scoreBoard_errors');
+export default class Document {
+  // ScoreBoard properties ----------------------------------------------------
+  /**
+   * scoreBoard
+   * @property
+   * @type Element
+   */
+  scoreBoardElement = this.$("scoreBoard");
+  /**
+   * current game's word per minute counter element
+   * @property
+   * @type {Element}
+   */
+  wordPerMinuteElement = this.$("scoreBoard_WPM");
+  /**
+   * the current game's errors element'
+   * @property
+   * @type {Element}
+   */
+  errorsElement = this.$("scoreBoard_errors");
 
+  // other properties ----------------------------------------------------
 
-    // other properties ----------------------------------------------------
+  /**
+   * the text in the middle of the screen that changes based on what user is doing
+   * at first it will tell user to focus and after that it will guide user though the game
+   * @property
+   * @type Element
+   */
+  pageGuideElement = this.$("pageGuide");
+  /**
+   * input files
+   * @property
+   * @type {Element}
+   */
+  inputElement = this.$("input");
 
-    /**
-     * the text in the middle of the screen that changes based on what user is doing
-     * at first it will tell user to focus and after that it will guide user though the game
-     * @property
-     * @type Element
-     */
-    pageGuideElement = this.$("pageGuide");
-    /**
-     * input files
-     * @property
-     * @type {Element}
-     */
-    inputElement = this.$("input");
+  /**
+   * @constructor
+   * the constructor will make changes to make the Doc ready for player to start the game
+   */
+  constructor() {
+    this.pageGuideElement.textContent = "GET READY TO PLAY!";
+  }
 
+  /**
+   * Just making life easier and adding a way to get dom elements faster
+   * @param elementId : string
+   * @returns {Element}
+   * @method
+   */
+  $(elementId) {
+    return document.querySelector("#" + elementId);
+  }
 
-    /**
-     * @constructor
-     * the constructor will make changes to make the Doc ready for player to start the game
-     */
-    constructor()
-    {
-        this.pageGuideElement.textContent = "GET READY TO PLAY!";
-    }
+  /**
+   * Will reset the scoreboard to its original state
+   * @method
+   */
+  scoreBoardReset() {
+    this.wordPerMinuteElement.textContent = 0;
+    this.errorsElement.textContent = 0;
+  }
 
+  /**
+   * Calls the scoreboardReset method of this document
+   * Changes the page guide element to game startup
+   * Resets the input element's value
+   * @method
+   */
+  gameInit() {
+    this.scoreBoardReset();
+    this.pageGuideElement.textContent = "Stay Focused";
+    this.inputElement.value = "";
+  }
 
-    /**
-     * Just making life easier and adding a way to get dom elements faster
-     * @param elementId : string
-     * @returns {Element}
-     * @method
-     */
-    $(elementId)
-    {
-        return document.querySelector("#"+elementId)
-    }
-
-    /**
-     * Will reset the scoreboard to its original state
-     * @method
-     */
-    scoreBoardReset()
-    {
-        this.wordPerMinuteElement.textContent = 0;
-        this.errorsElement.textContent = 0
-    }
-
-    /**
-     * Calls the scoreboardReset method of this document
-     * Changes the page guide element to game startup
-     * Resets the input element's value
-     * @method
-     */
-    gameInit() {
-        this.scoreBoardReset();
-        this.pageGuideElement.textContent = "Stay Focused";
-        this.inputElement.value = "";
-    }
-
-    /**
-     * Changes the page guide element to paased state
-     * Resets the input element's value
-     * In short, it will pause the game
-     * @method
-     */
-    pauseGame()
-    {
-        this.pageGuideElement.textContent = "PAUSED";
-        this.inputElement.value = "";
-    }
+  /**
+   * Changes the page guide element to paased state
+   * Resets the input element's value
+   * In short, it will pause the game
+   * @method
+   */
+  pauseGame() {
+    this.pageGuideElement.textContent = "PAUSED";
+    this.inputElement.value = "";
+  }
 }
--- scripts/Game.js
+++ scripts/Game.js
@@ -1,131 +1,125 @@
 import Document from "./Doc.js";
 
 export default class Game {
-    /**
-     * These are the states that the game is currently in.
-     * @type {{WAITING_TO_START: number, IN_PROGRESS: number, PAUSED: number, FINISHED: number}}
-     */
-    static State = {
-        WAITING_TO_START: 0,
-        IN_PROGRESS: 1,
-        PAUSED: 2,
-        FINISHED: 3
-    };
+  /**
+   * These are the states that the game is currently in.
+   * @type {{WAITING_TO_START: number, IN_PROGRESS: number, PAUSED: number, FINISHED: number}}
+   */
+  static State = {
+    WAITING_TO_START: 0,
+    IN_PROGRESS: 1,
+    PAUSED: 2,
+    FINISHED: 3,
+  };
 
-    /**
-     * @type {Game.State}
-     * @property
-     */
-    state;
+  /**
+   * @type {Game.State}
+   * @property
+   */
+  state;
 
-    /**
-     * Holds the document that game will control
-     * @type {Document}
-     * @property
-     */
-    doc
+  /**
+   * Holds the document that game will control
+   * @type {Document}
+   * @property
+   */
+  doc;
 
+  /**
+   * changes the state of the game
+   * sets the document
+   * initializes the game
+   * @constructor
+   * @param {Document} Document
+   */
+  constructor(Document) {
+    // changing state of the game
+    this.state = Game.State.WAITING_TO_START;
 
-    /**
-     * changes the state of the game
-     * sets the document
-     * initializes the game
-     * @constructor
-     * @param {Document} Document
-     */
-    constructor(Document) {
-        // changing state of the game
-        this.state = Game.State.WAITING_TO_START;
+    // setting the input field
+    this.doc = Document;
 
-        // setting the input field
-        this.doc = Document;
+    // game init
+    this.initGame();
+  }
 
-        // game init
-        this.initGame();
-    }
+  /**
+   * initializes the game with adding event listeners to input field
+   * and then the input field can track the changes in user behaviour and do what is needed
+   * @method
+   */
+  initGame() {
+    // Binding the event listener function reference
+    this.doc.inputElement.addEventListener(
+      "focusin",
+      this.inputFocusInListener.bind(this)
+    );
+    this.doc.inputElement.addEventListener(
+      "focusout",
+      this.inputFocusOutListener.bind(this)
+    );
+  }
 
-    /**
-     * initializes the game with adding event listeners to input field
-     * and then the input field can track the changes in user behaviour and do what is needed
-     * @method
-     */
-    initGame() {
-        // Binding the event listener function reference
-        this.doc.inputElement.addEventListener('focusin', this.inputFocusInListener.bind(this));
-        this.doc.inputElement.addEventListener('focusout', this.inputFocusOutListener.bind(this));
-    }
+  /**
+   * Will listen for focusin
+   * and starts the game if it is not already started abd resumes the game if it is paused
+   * @method
+   */
+  inputFocusInListener() {
+    this.stateToInProgress();
+    this.doc.gameInit();
+  }
 
+  /**
+   * Will track the focusout event and pauses the game if it is playing
+   * if the game has ended this will do nothing
+   * @method
+   */
+  inputFocusOutListener() {
+    this.pause();
+  }
 
-    /**
-     * Will listen for focusin
-     * and starts the game if it is not already started abd resumes the game if it is paused
-     * @method
-     */
-    inputFocusInListener()
-    {
-        this.stateToInProgress()
-        this.doc.gameInit();
-    }
+  /**
+   * Will pause the game
+   * @method
+   */
+  pause() {
+    // Setting games state to paused
+    this.stateToPaused();
 
-    /**
-     * Will track the focusout event and pauses the game if it is playing
-     * if the game has ended this will do nothing
-     * @method
-     */
-    inputFocusOutListener()
-    {
+    // Make the document to change its state to paused
+    this.doc.pauseGame();
+  }
 
-        this.pause()
-    }
+  /**
+   * changes state of the game to in progress the game
+   * @method
+   */
+  stateToInProgress() {
+    this.state = Game.State.IN_PROGRESS;
+  }
 
-    /**
-     * Will pause the game
-     * @method
-     */
-    pause()
-    {
-        // Setting games state to paused
-        this.stateToPaused();
+  /**
+   * changes state of the game to paused
+   * @method
+   */
+  stateToPaused() {
+    this.state = Game.State.PAUSED;
+  }
 
-        // Make the document to change its state to paused
-        this.doc.pauseGame();
-    }
+  /**
+   * changes state of the game to finished
+   * @method
+   */
+  stateToFinished() {
+    this.state = Game.State.FINISHED;
+  }
 
-
-    /**
-     * changes state of the game to in progress the game
-     * @method
-     */
-    stateToInProgress()
-    {
-        this.state = Game.State.IN_PROGRESS;
-    }
-
-
-    /**
-     * changes state of the game to paused
-     * @method
-     */
-    stateToPaused()
-    {
-        this.state = Game.State.PAUSED;
-    }
-
-    /**
-     * changes state of the game to finished
-     * @method
-     */
-    stateToFinished()
-    {
-        this.state = Game.State.FINISHED;
-    }
-
-    /**
-     * changes state of the game to waiting to start
-     * @method
-     */
-    stateToWaitingToStart()
-    {
-        this.state = Game.State.WAITING_TO_START;
-    }
+  /**
+   * changes state of the game to waiting to start
+   * @method
+   */
+  stateToWaitingToStart() {
+    this.state = Game.State.WAITING_TO_START;
+  }
 }
